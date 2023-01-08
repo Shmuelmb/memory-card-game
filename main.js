3588cea118f06ec270cd8e78107e45f9c6041f90
shmuel c <shmuel.cohen0@gmail.com>
2023-01-08 19:43:44 +0200

    finishim

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function shuffleArray(array) {
+function shuffleArray(array) { 
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
@@ -13,34 +13,58 @@ const cardsArray = [
     '&#128545;','&#128545;',
     '&#129313;','&#129313;']
 
-shuffleArray(cardsArray)
+shuffleArray(cardsArray) // shuffle all emoji's
 const inner = document.querySelectorAll('.inner')
-const back = document.querySelectorAll('.back')
+const backs = document.querySelectorAll('.back ')
+const modal = document.querySelector('.modal')
+modal.addEventListener('click', () =>{ //after win the game this func realod the page
+    location.reload()
+})
+
 let count = 0
-back.forEach(element => {
+backs.forEach(element => { //enter all emoji's to cards
     element.innerHTML = cardsArray[count]
     count += 1
-
 })
 
-let chance = []
-inner.forEach( event => {
-    const innerValue = parseInt(event.attributes.value.value) + 1
-    event.addEventListener('click', () => {
-        for (i = 1; i < 13; i++){
-            const Back = document.querySelector(`.back${i}`)
-            if (innerValue  === i ){
-                Back.style.transform = "none"
-                chance.push(Back.innerHTML)
-            } if (chance.length === 2){
 
+let winner = [] //this array counts all correct guesses 
+let chance = [] //this array temporarily stores the 2 selected guesses
+inner.forEach(event => { 
+    event.addEventListener('click',() => {
+        const innerValue = parseInt(event.attributes.value.value) + 1 // make sure the current element by id
+        const backItem = document.querySelector(`.back${innerValue}`)
+        backItem.style.transform = 'none'
+        chance.push(backItem)
+        if (chance.length === 2){
+            if (chance[1] === chance[0]){
+                chance.pop()
+            }else if (chance[1].innerHTML === chance[0].innerHTML){
+                setTimeout(() => {
+                    chance[1].style.backgroundColor = 'green'
+                    chance[0].style.backgroundColor = 'green'
+                    chance = []
+                    winner.push(' ')
+                },500)
+                
+            } 
+            else {
+                setTimeout(function(){
+                    chance[1].style.transform = 'rotateY(180deg)'
+                    chance[0].style.transform = 'rotateY(180deg)'
+                    chance = []
+                },1000)
             }
-        } 
-    })
-})
+        }else if( chance.length > 2){
+            let y = chance.slice(2)
+            y.forEach((event) => {event.style.transform = 'rotateY(180deg)'})
+           
+        } else if (winner.length === 5){
+            setTimeout(() => {
+                modal.style.display = 'block'
+            },2000)
+            
+        }
+    }) 
+}) 
 
-    
-// קודם כל צריך לבדוק איך הופכים כרטיס - בוצע
-// אחר כך צריך לבדוק איך לגרום לכרטיס להתהפך לאחר שתי ניחושים
-// לאחר מכן לגלות איך לבודק תוכן של כרטיסים
-// אחר כך לבדוק איך להשאיר את הכרטיסים ששווים ולהפוך את כל השאר
